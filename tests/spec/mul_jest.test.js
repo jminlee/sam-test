1b99680f4396d8166592ce5abbaf2da539c84467
Jaemin Lee <jaeminyx@Jaemins-MacBook-Pro.local>
2018-09-13 15:06:58 +0900

    add mul tests

---
+++ tests/spec/mul_jest.test.js
@@ -0,0 +1,5 @@
+const sum = require('../../lambda/jest_mul_test_handler');
+
+test('multiplies 2 * 3 to equal 6', () => {
+  expect(mul(2, 3)).toBe(6);
+});
---
+++ tests/test_jest_mul_handler.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../node_modules/.bin/jest -t spec/mul_jest_test.js
