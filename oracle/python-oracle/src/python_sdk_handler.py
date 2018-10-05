085cc3f394947efb4696c2e6817202b5aa7afe8a
Jaemin Lee <jaeminyx@Jaemins-MacBook-Pro.local>
2018-10-05 16:13:32 +0900

    dasdzxcxz

--- oracle/python-oracle/src/python_sdk_handler.py
+++ oracle/python-oracle/src/python_sdk_handler.py
@@ -1,25 +1,21 @@
-import numpy
+import numpy as np
 import boto3
 import json
 
 def main_handler(event, callback):
 
-    check_numpy()
     matrix_json = get_matrix_json()
 
     matrix_a = np.array(matrix['matrix_a'])
 
     matrix_b = np.array(matrix['matrix_b'])
 
-    result = numpy.matmul(matrix_a, matrix_b)
+    result = np.matmul(matrix_a, matrix_b)
 
     print(result)
 
     return "Cool"
 
-def check_numpy():
-    np_arr = numpy.array([])
-    print(np_arr)
 
 def get_matrix_json():
     s3_client = boto3.client("s3")
