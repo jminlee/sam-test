4159306389531d24193e7025ad045fa1f5490f8e
Jaemin Lee <jaeminyx@Jaemins-MacBook-Pro.local>
2018-10-05 14:42:37 +0900

    qweasdscxzczx

--- oracle/python-oracle/src/python_sdk_handler.py
+++ oracle/python-oracle/src/python_sdk_handler.py
@@ -4,9 +4,20 @@ import boto3
 def main_handler(event, callback):
 
     check_numpy()
+    print(get_matrix_json())
 
-    return callback()
+    return "Cool"
 
 def check_numpy():
     np_arr = numpy.array([])
     print(np_arr)
+
+def get_matrix_json():
+    s3_client = boto3.client("s3")
+
+    responce = s3_client.get_object(
+        Bucket="sam.mat.test"
+        , Key="Event/Matrix.json"
+    )
+
+    print(responce)
