4882efa6dc6afa5afc9cd8d1615e4a4e91da166f
Jaemin Lee <jaeminyx@Jaemins-MacBook-Pro.local>
2018-10-05 11:56:44 +0900

    weasdasdas

--- oracle/python-oracle/src/python_sdk_handler.py
+++ oracle/python-oracle/src/python_sdk_handler.py
@@ -2,7 +2,11 @@ import numpy
 import boto3
 
 def main_handler(event, callback):
-    np_arr = np.array()
-    print(event)
-    print(np_arr)
+    
+    check_numpy()
+
     return callback()
+
+def check_numpy():
+    np_arr = np.array()
+    print(np_arr)
