3e2695221c325ea030bc9a8b7748b9ef89b18857
Jaemin Lee <jaeminyx@Jaemins-MacBook-Pro.local>
2018-10-08 17:11:42 +0900

    asdxcxcwqeasd

--- oracle/java-oracle/src/main/java_sdk_handler.java
+++ oracle/java-oracle/src/main/java_sdk_handler.java
@@ -1,7 +1,7 @@
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 
-public class Hello implements RequestHandler<Request, Response> {
+public class SdkHandler implements RequestHandler<Request, Response> {
 
     public Response handleRequest(Request request, Context context) {
         return new Response();
